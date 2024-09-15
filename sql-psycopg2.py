22d55de212415c757711771f23926ce849d73607
Alice Chan <alicebay10@yahoo.co.uk>
2024-09-15 14:51:52 +0000

    installed SQL Alchemy.

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -8,13 +8,13 @@ connection = psycopg2.connect(database="chinook")
 cursor = connection.cursor()
 
 #Query 1 - select all records from the "Artist" table
-#cursor.execute('SELECT * FROM "Artist"')
+cursor.execute('SELECT * FROM "Artist"')
 
 # Query 2 - select only the "Name" column from the "Artist" table
 #cursor.execute('SELECT "Name" FROM "Artist"')
 
 # Query 3 - select only "Queen" from the "Artist" table
-#cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
 
 # Query 4 - select only by "ArtistId" #51 from the "Artist" table
 #cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
@@ -23,13 +23,13 @@ cursor = connection.cursor()
 #cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
 # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-cursor.execute('SELECT * FROM "Track" WHERE "Composer" =%s', ["Queen"])
-
+#cursor.execute('SELECT * FROM "Track" WHERE "Composer" =%s', ["Queen"])
+#
 # fetch the results (mulitple)
-# results = cursor.fetchall()
+results = cursor.fetchall()
 
 # fetch the result (single)
-results = cursor.fetchone()
+#results = cursor.fetchone()
 
 # close the connection
 connection.close()
